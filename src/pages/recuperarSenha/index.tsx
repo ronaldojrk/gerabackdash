4f000d4596a9dc173de22a1d1aa9915330d81504
Ronaldo <ronaldo.jrk@gmail.com>
2022-12-07 11:22:31 -0300

    recuperar senha

--- src/pages/recuperarSenha/index.tsx
+++ src/pages/recuperarSenha/index.tsx
@@ -1,26 +1,53 @@
 import React from 'react'
 import Styles from './recuperarSenha.module.scss';
+import { useContext, useEffect, useRef, useState } from "react";
+import { createUserWithEmailAndPassword, sendPasswordResetEmail } from "firebase/auth";
+import { auth } from '../../../firebase-config';
+import 'react-toastify/dist/ReactToastify.css';
 
-
+import { ToastContainer, toast } from 'react-toastify';
 export default function RecuperarSenha() {
 
-  return (
-    <div className={Styles.geral}>
-        <div className={Styles.div1}></div>
-        <div className={Styles.div2e3}>
-            <div className={Styles.div2}></div>
+
+    async function handleSubmit() {
+        sendPasswordResetEmail(auth, email).then(async (userCredential) => {
+
+            toast.info('verifique seu email!', {
+                position: toast.POSITION.TOP_RIGHT
+            });
+        })
+
+
+    }
+
+    const [email, setEmail] = useState("");
+    return (
+        <div className={Styles.geral}>
+            <div className={Styles.div1}></div>
+            <div className={Styles.div2e3}>
+                <div className={Styles.div2}></div>
                 <div className={Styles.senha}>
+                    <ToastContainer />
                     <h1>Recupere sua senha!</h1>
                     <label htmlFor="">Digite seu email:</label><br />
-                    <input type="text" />
+                    <input type="text" value={email}
+                        onChange={(e) => setEmail(e.target.value)} />
                     <div className={Styles.divButton}>
-                        <a href="../"><button className={Styles.button2}><p>Enviar</p></button></a>    
+                        <button className={Styles.button2}
+                            onClick={() => {
+
+                                if (email.length > 0) {
+                                    handleSubmit()
+                                }
+
+                            }}
+                        ><p>Enviar</p></button>
                     </div>
                 </div>
-            <div className={Styles.div3}></div>
+                <div className={Styles.div3}></div>
+            </div>
+            <div className={Styles.div4}></div>
         </div>
-        <div className={Styles.div4}></div>
-    </div>
 
-  )
+    )
 }
